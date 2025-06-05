ba522241458f0e50ec0934d6e40dea183d6ad7f6
24f2008196 <24f2008196@ds.study.iitm.ac.in>
2025-06-05 20:17:11 +0530

    Delete ecofinds directory

--- ecofinds/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
